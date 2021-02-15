e2710cb3337a110f457b7fef1613e8aef9aefc7e
Laura <lauraporto596@gmail.com>
2021-02-15 21:26:42 +0100

    Búsqueda por título correcta

--- js/main.js
+++ js/main.js
@@ -2,36 +2,55 @@ let valor = document.getElementById("buscador");
 
 let key = "c0b6dea31a9d647a6b7d1eafa59bacaa";
 
-let criterio = "";
+let criterio = "movie";
+
+let recurso = "search";
 
 let base_url = `http://api.themoviedb.org/3/${recurso}`;
 
+let pelicula = null;
+
+let number = 0;
 
 const call = async(url) => {
     let res = await axios.get(url);
 
     if (!res.data.results) {
         error = new Error("La URL no es correcta");
-        return error.toSource();
-    }
+        return error;
+    };
 
     return res.data.results;
 };
 
-const search = async() => {
-    let query = valor.nodeValue;
 
-    let url = `${base_ur}/${criterio}?api_key=${key}&query=${query}`;
-    print("Cargando...");
+const buscador = async() => {
+    let query = valor.value;
 
-    let pelis = await call(url);
+    let url = `${base_url}/${criterio}?api_key=${key}&query=${query}`;
 
+    if (query == number) {
+        recurso = find;
+        return pelicula.external_id;
+    }
+
+    pelicula = await call(url);
+
+    pintar(pelicula);
 
 };
 
-const print = async(coleccionPintar) => {
+const pintar = async(coleccionPintar) => {
 
-    document.getElementById("contenedor").innerHTML = JSON.stringify(coleccionPintar);
+    let content = document.getElementById("contenedor");
 
-    return;
+    coleccionPintar.forEach(pelicula => {
+        console.log(pelicula);
+
+        content.innerHTML += `</br><h2> ${pelicula.title} </h2> <img src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}"></br>${pelicula.overview} </br>`;
+    })
+
+
+    return content;
+
 };
